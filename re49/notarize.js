7abc07a76522b5542946ad00baa81159dc0e601a
Prez Ivan <ivpcode@gmail.com>
2021-06-25 08:18:27 +0200

    Pinning

--- re49/notarize.js
+++ re49/notarize.js
@@ -6,7 +6,9 @@ const LiquidJS = require('liquidjs')
 const fs = require('fs-extra')
 const Utils = require('./lib/utils')
 const recursive = require("recursive-readdir");
-const { formatWithOptions } = require('util');
+//const IPFS = require('ipfs-core')
+const ipfsClient = require('ipfs-http-client')
+const { globSource } = require('ipfs-http-client')
 
 const NotarizationRepoDir = Path.join(__dirname,"notarizations")
 const DataDir = Path.join(__dirname,"data")
@@ -14,12 +16,17 @@ const TemplateDir = Path.join(__dirname,"template/dist")
 
 let serial = process.argv[2]
 let model = process.argv[3]
-let producer = process.argv[4];
+
+let producer = "MASOLINI_EREDI"
+
+if (process.argv.length > 4)
+    producer = process.argv[4];
+
 
 (async ()=>{
     try {
         //:: Copia la folder di template nella destination
-        const OutputDir = fs.mkdtempSync("re40-notarizer")//Path.join(NotarizationRepoDir,Utils.FormatDate(new Date(),"YYYY-MM-DD_HH.mm.ss"))
+        const OutputDir = fs.mkdtempSync("re40-notarizer")
         fs.removeSync(OutputDir)        
         fs.copySync(TemplateDir,OutputDir,{overwrite:true})
 
@@ -48,8 +55,17 @@ let producer = process.argv[4];
             }
             
         });
-        
-        console.log("Builded and notarized in folder: "+OutputDir)
+
+        let ipfs = ipfsClient.create()
+        const  {cid}  = await ipfs.add(globSource(OutputDir,{recursive:true}))
+
+        let PinnedNotarization = Path.join(NotarizationRepoDir,cid.string)
+        fs.copySync(OutputDir,PinnedNotarization,{overwrite:true})
+
+        fs.removeSync(OutputDir)
+
+        console.log("Builded and notarized in folder: "+PinnedNotarization)
+        console.log("CID: "+cid.string+"\n")
     }
     catch (Ex) {
         console.log(Ex) 
